bda17a4f27ad1af1da1312aabf92387f712a0168
samelnmu <samelnmu@gmail.com>
2024-11-07 12:24:47 +0300

    attemp1

---
+++ user_input.py
@@ -0,0 +1,10 @@
+name = input("What is your name?")
+print(name)
+
+age = input("How old are you?")
+print(age)
+
+location =input("Where do you live? City:Country")
+print(location)
+
+welcome = print("Welcome" + name + "!")
